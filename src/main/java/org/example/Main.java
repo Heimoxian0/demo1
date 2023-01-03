867d41520bd102987e3a12d69a5ba5ff330e6a89
zyw <1044351131@qq.com>
2023-01-03 10:09:32 +0800

    first

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    static int num=103;
+    static char z='g';
+
+    public static void main(String[] args) {
+
+        System.out.println(z==num);
+//        System.out.println("加入一行新代码");
+    }
+}
