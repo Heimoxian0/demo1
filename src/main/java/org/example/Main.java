497471b033cf0b0cfb8acc72a5028d024c89ae37
zyw <1044351131@qq.com>
2023-01-03 10:36:55 +0800

    second change

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,5 +11,7 @@ public class Main {
         System.out.println("second");
         System.out.println("third");
         System.out.println("fourth");
+        System.out.println("hello hotfix");
+
     }
 }
