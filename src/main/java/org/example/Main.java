d406916dd9620879662fd2c67060546062f2a605
zyw <1044351131@qq.com>
2023-01-03 10:21:22 +0800

    first

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,5 +10,6 @@ public class Main {
 //        System.out.println("加入一行新代码");
         System.out.println("second");
         System.out.println("third");
+        System.out.println("fourth");
     }
 }
